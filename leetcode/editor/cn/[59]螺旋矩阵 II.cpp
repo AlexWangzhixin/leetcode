15738c4492ee10792616e930bca70895e143c11b
AlexWangzhixin <alexwangzhixin44@gmail.com>
2024-07-06 16:11:49 +0800

    sd

---
+++ leetcode/editor/cn/[59]螺旋矩阵 II.cpp
@@ -0,0 +1,40 @@
+//给你一个正整数 n ，生成一个包含 1 到 n² 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：n = 3
+//输出：[[1,2,3],[8,9,4],[7,6,5]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：[[1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 20 
+// 
+//
+// Related Topics 数组 矩阵 模拟 👍 1295 👎 0
+
+
+#include "include/headers.h"
+#include<vector>
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
